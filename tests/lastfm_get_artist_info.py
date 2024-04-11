aa79375972ef741ddf90957dc937a35410278e30
Omen4Dead <69041761+Omen4Dead@users.noreply.github.com>
2024-04-11 19:12:18 +0500

    load artist
    Немного поработал над отлове ошибок при парсинге

--- tests/lastfm_get_artist_info.py
+++ tests/lastfm_get_artist_info.py
@@ -16,8 +16,8 @@ artist_info = []
 
 with conn.cursor() as curs:
     curs.execute('''select lhd.artist_mbid , lhd.artist_name 
-    from test_db.lastfm_history_data lhd 
-    where date_trunc('day', lhd.dt_listen) = current_date - 2''')
+                      from test_db.lastfm_history_data lhd 
+                     where date_trunc('day', lhd.dt_listen) = current_date - 2''')
 
     for row in curs:
         params = {'api_key': cfg.lastfm_key,
@@ -27,17 +27,35 @@ with conn.cursor() as curs:
                   'lang': 'ru',
                   'username': cfg.lastfm_username
                   }
-        print(params)
+        # print(params)
         response = requests.get(url=url, params=params)
-        print(response.text)
-        df = pd.json_normalize(response.json()['artist'])
-        print(df.keys())
+        # print(response.text)
+        try:
+            df = pd.json_normalize(response.json()['artist'])
+        except KeyError as e:
+            print(e, ' -> Ошибка разбора JSON, вероятно, что артист с таким именем не нашелся')
+            break
+        # распечатываем список атрибутов после из нормализации
+        # print(df.keys())
+        try:
+            artist_name = df.name[0]
+            # artist_mbid = df.mbid[0]
+            artist_url = df.url[0]
+            tags = pd.json_normalize(df['tags.tag'][0])['name'].tolist()
+            dt_published = df['bio.published'][0]
+        except AttributeError and KeyError as e:
+            print(e, ' -> Ошибка разбора JSON, вероятно, что атрибут не пришел с источника')
+            continue
 
-        artist_info.append({'name': df.name[0],
-               # 'artist_mbid': df.mbid[0],
-               'artist_url': df.url[0],
-               'tags': pd.json_normalize(df['tags.tag'][0])['name'].tolist(),
-               'dt_published': df['bio.published'][0]
+        artist_info.append({'name': artist_name,
+               # 'artist_mbid': artist_mbid,
+               'artist_url': artist_url,
+               'tags': tags,
+               'dt_published': dt_published
                })
 
 print(artist_info)
+
+artists_df = pd.DataFrame(artist_info)
+
+artists_df.to_csv('./files/artists.csv')
